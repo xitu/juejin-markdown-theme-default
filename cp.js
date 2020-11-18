ccfbdb7bf124ed82967e2a5d4c3bccc83d4c529f
Linxs <yunfax@outlook.com>
2020-11-18 10:56:51 +0800

    feat: 更新渲染逻辑支持表格、GFM、KeTeX

--- cp.js
+++ cp.js
@@ -1,4 +1,3 @@
 const copyFile = require('./tools/copyFile.js');
 
-// 注意更改文件名和仓库地址
 copyFile('juejin.scss', '../juejin-markdown-theme-cyanosis/');
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const run = require('./tools/server.js');
 
-// 参数：样式文件，注意更改文件名
+// 参数：样式文件
 run('juejin.scss');
 
